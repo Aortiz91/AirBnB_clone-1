8dbe26a3487d88889792f7ff578cee329371a2b4
Andy <3283@holbertonschool.com>
2022-01-29 03:32:17 +0000

    corrected comments and added second route

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,6 +10,8 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def homepage():
     return 'Hello HBNB!'
-
+@app.route("/hbnb", strict_slashes=False)
+def hbnbpage():
+    return 'HBNB'
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
