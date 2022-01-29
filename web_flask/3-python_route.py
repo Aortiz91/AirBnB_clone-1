fe698cab0445eca2c061ed7b869fce12d6cf4b05
Andy <3283@holbertonschool.com>
2022-01-29 18:19:56 +0000

    Addded 3-5

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+    Script that starts a Flask web application
+"""
+
+
+from flask import Flask, escape
+
+app = Flask(__name__)
+"""
+    Instance of the class Flask, this is the WSGI application
+"""
+
+
+@app.route("/", strict_slashes=False)
+def homepage():
+    return 'Hello HBNB!'
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnbpage():
+    return 'HBNB'
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def textpage(text):
+    return 'C {}'.format(escape(text.replace("_", " ")))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>")
+def textdefaultpage(text="is_cool"):
+    return 'Python {}'.format(escape(text.replace("_", " ")))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ web_flask/5-number_template.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+    Script that starts a Flask web application
+"""
+
+
+from flask import Flask, escape, render_template
+
+app = Flask(__name__)
+"""
+    Instance of the class Flask, this is the WSGI application
+"""
+
+
+@app.route("/", strict_slashes=False)
+def homepage():
+    return 'Hello HBNB!'
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnbpage():
+    return 'HBNB'
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def textpage(text):
+    return 'C {}'.format(escape(text.replace("_", " ")))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>")
+def textdefaultpage(text="is_cool"):
+    return 'Python {}'.format(escape(text.replace("_", " ")))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def numberpage(n):
+    return "{} is a number".format(escape(n))
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def htmlpage(n):
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
