01d5e534ac74ea65bee499e3aded1c39f12f7753
Andy <3283@holbertonschool.com>
2022-01-29 19:43:34 +0000

    Added 6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+    Script that starts a Flask web application
+"""
+
+
+from flask import Flask, escape, render_template
+
+app = Flask(__name__)
+"""
+    Instance of the class Flask, this is the WSGI application
+"""
+
+
+@app.route("/", strict_slashes=False)
+def homepage():
+    return 'Hello HBNB!'
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnbpage():
+    return 'HBNB'
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def textpage(text):
+    return 'C {}'.format(escape(text.replace("_", " ")))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>")
+def textdefaultpage(text="is_cool"):
+    return 'Python {}'.format(escape(text.replace("_", " ")))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def numberpage(n):
+    return "{} is a number".format(escape(n))
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def htmlpage(n):
+    return render_template('5-number.html', n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def odd_or_evenpage(n):
+    if (n % 2 is 0):
+        oddoreven = 'even'
+    else:
+        oddoreven = 'odd'
+    return render_template('6-number_odd_or_even.html',
+                           n=n, oddoreven=oddoreven)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
